f3cf86fe52941c4589435cdff35b338c9c1025ab
nigimitama <nigimitama16@gmail.com>
2019-12-04 09:00:27 +0900

    update

--- paiza/C058.py
+++ paiza/C058.py
@@ -1,16 +1,19 @@
 N, t, s  = input().split()
 N = int(N)
 
-for i in range(N):
-    s = s[1:] + s[0]
-    if s == t:
-        count_r = i+1
-        break
+if s == t:
+    print(0)
+else:
+    for i in range(N):
+        s = s[1:] + s[0]
+        if s == t:
+            count_r = i+1
+            break
 
-for i in range(N):
-    s = s[-1] + s[0:(N-1)]
-    if s == t:
-        count_l = i+1
-        break
+    for i in range(N):
+        s = s[-1] + s[0:(N-1)]
+        if s == t:
+            count_l = i+1
+            break
 
-print(min(count_r, count_l))
+    print(min(count_r, count_l))
