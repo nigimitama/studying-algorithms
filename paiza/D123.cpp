813e37dac73e68509660f9871aaa253d864dddfd
nigimitama <nigimitama16@gmail.com>
2020-01-06 07:04:39 +0900

    add

---
+++ paiza/D123.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    if (x < 10000) {
+        cout << 10000+x << endl;
+    } else {
+        cout << x << endl;
+    }
+}
