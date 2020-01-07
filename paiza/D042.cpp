e3a424fafe2dff306f83af2c5c44e33f6315e6e8
nigimitama <nigimitama16@gmail.com>
2020-01-08 07:23:40 +0900

    add

---
+++ paiza/D042.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b;
+    cin >> c >> d;
+    int s = a * d - b * c;
+    cout << s << endl;
+}
---
+++ paiza/D052.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int s = n * (n + 1) / 2;
+    cout << s << endl;
+}
+
+// int main() {
+//     int n, s = 0;
+//     cin >> n;
+//     for (int i=1; i <= n; i++) {
+//         s += i;
+//     }
+//     cout << s << endl;
+// }
