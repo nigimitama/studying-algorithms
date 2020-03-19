162236012131a8e1eb390fbefedd3d8273876d61
nigimitama <nigimitama16@gmail.com>
2020-03-20 00:26:24 +0900

    add

---
+++ paiza/D150.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x;
+    cin >> y;
+    if (x < y) {
+        cout << y - x << endl;
+    } else {
+        cout << "Thank you" << endl;
+    }
+}
