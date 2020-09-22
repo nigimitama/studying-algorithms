0b9fbc4f078e8ba550fdcdaec70a29ff03c23444
nigimitama <nigimitama16@gmail.com>
2020-09-22 11:56:40 +0900

    add

--- paiza/tB01.py
+++ paiza/tB01.py
@@ -1,12 +1,29 @@
 # https://paiza.jp/works/mondai/skillcheck_sample/long-table?language_uid=python3
+
+
+def get_index(i_start, delta, table):
+    indices = []
+    for i in range(i_start, i_start + delta):
+        if i >= len(table):
+            i = i - len(table)
+        indices.append(i)
+    return indices
+
+
 n, m = map(int, input().split())
 ab = [list(map(int, input().split())) for _ in range(m)]
 
 table = [0]*n
-
+n_customers = 0
 for a, b in ab:
-    b = b - 1
-    if sum(table[b:b+a]) == 0:
-        table[b:b+a] = [1]*a
+    i_start = b - 1
 
-print(sum(table))
+    indices = get_index(i_start, a, table)
+    is_empty = sum([table[i] for i in indices]) == 0
+
+    if is_empty:
+        n_customers += a
+        for i in indices:
+            table[i] = 1
+
+print(n_customers)
