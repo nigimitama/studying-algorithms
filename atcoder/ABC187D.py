7c8b5a5e6e092a772b236db6e53462fb84a1e05b
mitama <nigimitama16@gmail.com>
2021-01-03 01:01:44 +0900

    解説読んで再チャレンジした

--- atcoder/ABC187D.py
+++ atcoder/ABC187D.py
@@ -1,3 +1,4 @@
+# 1部WA/Timeout
 N = int(input())
 AB = [list(map(int, input().split())) for _ in range(N)]
 
@@ -17,4 +18,29 @@ for i in n_poeples_idx:
         count += 1
     # print(n_a, n_b)
 print(count)
-# 1部WA/Timeout
+
+"""
+解説
+https://atcoder.jp/contests/abc187/editorial/486
+
+1. X = (高橋氏の得る票数) − (青木氏の得る票数)とし、X>0になるようにすればよい
+2. i番目の町で演説すると高橋がA+Bだけ増えて青木がAだけ減るので、Xは2A+Bだけ増える
+3. Xが高い順に演説していけばいい
+よって
+↓
+"""
+# 修正後、AC
+N = int(input())
+AB = [list(map(int, input().split())) for _ in range(N)]
+X_gain = [(2*a+b) for a,b in AB]  # その町で演説したら得られるXの増分
+X_gain.sort(reverse=True)
+X = -sum([a for a,b in AB])   # 初期値（高橋が0、青木が全部）
+
+count = 0
+for i in range(N):
+    if X > 0:
+        break
+    else:
+        X += X_gain[i]
+        count += 1
+print(count)
